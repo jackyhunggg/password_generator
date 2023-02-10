a7b4cfe07e60feacf22da9918d8f844423a6fc63
Jacky Hung <jacky85725@gmail.com>
2023-02-11 00:05:23 +0800

    setup the project

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const app = express();
+const port = 3000;
+
+// setting template engine
+app.engine('handlebars',exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+// setting routes
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+//starts the express server and listening for connection
+app.listen(port, () => {
+    console.log(`Express app listening on port ${port}`)
+})
